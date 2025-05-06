10313b635641ed4e12409d84fb7ec8f2943f3999
snorkelingcode <daneknudsen3d@gmail.com>
2025-05-05 20:38:23 -0500

    Initial commit with Git LFS for Unreal Engine

---
+++ Source/NEUROSYNC/NEUROSYNCGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NEUROSYNCGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ANEUROSYNCGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ANEUROSYNCGameMode();
+};
+
+
+
